fcecdabda8a6c848737a61f2f758ef68b6848023
Avram Razvan <razvan@Avrams-MacBook-Pro-2.local>
2015-12-18 12:49:16 +0200

    First commit

---
+++ src/fromjs/core.cljs
@@ -0,0 +1,35 @@
+(ns fromjs.core
+  (:require [reagent.core :as reagent :refer [atom]]))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+;;MODEL
+(defonce app-state (atom {:text "Hello world!"}))
+
+
+;;CONTROLLER
+(defn ^:extern change_title [title]
+  (swap! app-state assoc :text title))
+
+
+;;VIEWS
+(defn hello-world []
+  [:h1 (:text @app-state)])
+
+
+
+
+
+(reagent/render-component [hello-world]
+                          (. js/document (getElementById "app")))
+
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
